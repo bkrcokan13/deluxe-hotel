a2d1dcc4c99b0046e52cde7bb7a051dfe975c521
bkrcokan13 <132247985+bkrcokan13@users.noreply.github.com>
2023-10-28 03:35:52 +0300

    Update index.html, style.css
    Added info box

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function galleryPictures() {
+
+    let pictures = [
+        "/img/2.jpg",
+        "/img/3.jpg",
+        "/img/4.jpg",
+        "/img/5.jpg",
+        "/img/6.jpg",
+        "/img/7.jpg",
+        "/img/8.jpg",
+        "/img/9.jpg",
+    ];
+    
+
+    let galleryBox = document.querySelectorAll(".flex-image-item");
+
+    galleryBox.forEach((element, index) => {
+
+        console.log(element);
+        element.style.backgroundImage = `url(${pictures[index]})`;
+    });
+}
+
+galleryPictures();
